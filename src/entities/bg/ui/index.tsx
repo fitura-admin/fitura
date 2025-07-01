26560c5f57db4d05d234f32e209371d8a3266980
fitura-admin <blagikhpavel@outlook.com>
2025-07-01 09:39:37 +0300

    Add files via upload

--- src/entities/bg/ui/index.tsx
+++ src/entities/bg/ui/index.tsx
@@ -1,9 +1,13 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useRef } from "react";
 
 import classes from "./bg.module.scss";
 
-export default function PageBg() {
+interface Props {
+  setActive: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function PageBg({ setActive }: Props) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
@@ -17,7 +21,14 @@ export default function PageBg() {
 
   return (
     <div className={classes.container}>
-      <video muted loop playsInline preload="metadata" ref={videoRef}>
+      <video
+        muted
+        loop
+        playsInline
+        preload="metadata"
+        ref={videoRef}
+        onCanPlayThrough={() => setActive(false)}
+      >
         <source src="/bg-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
