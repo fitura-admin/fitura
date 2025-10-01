e878757d05d8da2dd5f7c57f14066104c98d7c80
plecenb <zhavoronka2019@gmail.com>
2025-10-01 17:23:47 +0300

    Update membership.const.ts

--- src/entities/membership/model/membership.const.ts
+++ src/entities/membership/model/membership.const.ts
@@ -4,13 +4,13 @@ export const membershipCards: IMembershipCard[] = [
   {
     title: "Duo",
     subTitle: "Train, recover, and thrive together \n— full access for two",
-    price: "140",
+    price: "129.5",
   },
   {
     title: "Junior",
     subTitle:
       "Smart start for ages 16-18: full gym, spa, classes, app access, and more",
-    price: "55",
+    price: "50",
   },
 ];
 
@@ -19,20 +19,20 @@ export const membershipSlides: IMembershipCard[] = [
     title: "Full\u00A0Access",
     subTitle:
       "All-inclusive membership with gym, spa, classes, recovery, and more",
-    price: "75",
+    price: "69.5",
     moreContent: true,
   },
   {
     title: "Duo",
     subTitle: "Train, recover, and thrive together \n— full access for two",
-    price: "140",
+    price: "129.5",
     moreContent: true,
   },
   {
     title: "Junior",
     subTitle:
       "Smart start for ages 16-18: full gym, spa, classes, app access, and more",
-    price: "55",
+    price: "50",
     moreContent: true,
   },
 ];
@@ -41,6 +41,6 @@ export const membershipBigCard: IMembershipCard = {
   title: "Full\u00A0Access",
   subTitle:
     "All-inclusive membership with gym, spa, classes, recovery, and more",
-  price: "75",
+  price: "69.5",
   moreContent: true,
 };
