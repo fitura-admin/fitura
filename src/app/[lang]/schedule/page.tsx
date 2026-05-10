1c6070c1d77fb5c548666706a52f979e319be0cd
leSchb <kurultae@yandex.ru>
2026-05-10 22:03:36 +0300

    add schedule iframe

---
+++ src/app/[lang]/schedule/page.tsx
@@ -0,0 +1,5 @@
+import HomePage from "~/src/pages-components/homepage";
+
+export default function Page() {
+  return <HomePage />;
+}
--- src/entities/header/ui/nav-section/nav/index.tsx
+++ src/entities/header/ui/nav-section/nav/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 import classes from "./nav.module.scss";
 import HeaderNavButton from "./button";
@@ -9,12 +10,27 @@ interface Props {
 }
 
 export default function HeaderNav({ children }: Props) {
+  const [withSchedule, setWithSchedule] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (window.location.href.includes("schedule")) {
+      setWithSchedule(true);
+    }
+  }, []);
+
   return (
     <nav className={`flex-row align-center ${classes.container}`}>
       {headerNavButtons.map((item, index) => {
         return <HeaderNavButton key={index} {...item} />;
       })}
       {children}
+      {withSchedule && (
+        <iframe
+          src="https://fiturafitness-ebooking.flybyglobal.com/"
+          title="Fitura Fitness Online Booking"
+          className={classes.frame}
+        ></iframe>
+      )}
     </nav>
   );
 }
